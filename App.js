0c2c04a25d72c8966db52f0d07e1476f70981285
reyesrannie <117341670+reyesrannie@users.noreply.github.com>
2022-12-05 10:28:27 +0800

    Training

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Dimensions,
+  StyleSheet,
+  Text, 
+  View, 
+  SafeAreaView, 
+  StatusBar, 
+  Alert, 
+  Platform
+} from 'react-native';
 
 export default function App() {
+  console.log(Dimensions.get('screen'));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     <View style={styles.viewFormat}></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+   paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0
   },
+  viewFormat: {
+    backgroundColor: 'dodgerblue',
+    width: "50%",
+    height: 70
+  }
 });
